80e9f8dd37d8a87ee294e505a1fd04ffbac030af
Fernando <ryumeisen@gmail.com>
2016-07-25 12:04:54 -0600

    Archivos de configuracion
    Agregado archivo de configuracion Grun y el package.json

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      js: {
+        files: ['app-dev/app/**/*.js'],
+        options: {
+          livereload: true
+        },
+      //tasks: ['default'],
+      },
+      html: {
+        files: ['app-dev/**/*.html'],
+        options: {
+          livereload: true
+        },
+      }
+    },
+    uglify: {
+      src: {
+        files: {
+          'dest/output.min.js': ['src/input.js']
+        }
+      }
+    },
+    copy: {
+      config: {
+        files: [{
+          expand: true,
+          src: ['Gruntfile.js','package.json'],
+          dest: 'app-dev/'
+        }]
+      },
+      js: {
+        files: [
+          // includes files within path and its sub-directories 
+          {
+            expand: true,
+            src: ['app-dev/**'],
+            dest: 'dist/'
+          }
+        ],
+      },
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'release/css',
+          src: ['app-dev/assets/css/*.css', 'app-dev/assets/css/!*.min.css'],
+          dest: 'dist/assets/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    htmlmin: { // Task 
+      dist: { // Target 
+        options: { // Target options 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: { // Dictionary of files 
+          'dist/index.html': 'src/index.html', // 'destination': 'source' 
+          'dist/contact.html': 'src/contact.html'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3001,
+          livereload: true,
+          open: true,
+          base: 'app-dev/'
+        }
+      }
+    }
+  });
+
+
+  // Ver cambios en los archivos
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Copiar archivos
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Compresor de archivos HTML
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  // Compresor de archivos JS
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Compresor de archivos CSS
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Conectar a un servidor we
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  require('connect-livereload')();
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect', 'copy:config', 'watch']);
+
+
+};
